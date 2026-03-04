440582591f95262a1494375205cfcc51db892095
ATOMASHOFF <atomashoffgaming@gmail.com>
2026-03-04 17:57:14 +0530

    Fix line error in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login, getMe, changePassword } = require('../src/controllers/authController');
+const { login, getMe, changePassword } = require('../controllers/authController');
 const { authenticateToken } = require('../src/middleware/auth');
 
 router.post('/login', login);
