5529d173b42ec664b553824d38b4e277d819ea5d
ATOMASHOFF <atomashoffgaming@gmail.com>
2026-03-04 20:08:51 +0530

    Update API URL to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://unkidnaped-janine-addictively.ngrok-free.dev/api',
+  baseURL: 'https://atom-fitness-api.onrender.com/api',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 });
